12d8f255e7f85e798c008bbd1b987ccdb02a1288
Sabad <41366297+SabadModi@users.noreply.github.com>
2021-07-17 00:01:21 +0400

    update files

--- examination/home.php
+++ examination/home.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['id']) == true) header("location:../index.php");
 
 ?>
 <?php include("conn.php"); ?>
-<!-- MAO NI ANG HEADER -->
+<!-- HEADER -->
 <?php include("includes/header.php"); ?>
 
-<!-- UI THEME DIRI -->
+<!-- UI THEME -->
 <?php include("includes/ui-theme.php"); ?>
 
 <div class="app-main">
